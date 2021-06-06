32c3b91053e87f543bee786f3803a77be6ad1f49
MohammadReza Iranmanesh <imohammadreza.ir@gmail.com>
2021-06-06 15:21:54 +0430

    feat: add SSR sample component

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MetaTags from 'react-helmet';
 
 import logo from 'src/assets/logo.svg';
+import withSSRData from 'src/shared-components/withSSRData';
+
+import { getHomeTitle } from './requests';
 
 import './Home.scss';
 
-function Home() {
-  return (
-    <main className="container home">
-      <MetaTags title="divar-starter-kit 🚀" />
-      <img
-        className="home__logo"
-        src={logo}
-        alt="divar-stater-kit"
-      />
-      <p className="title">Running divar-starter-kit successfully.</p>
-    </main>
-  );
+class Home extends Component {
+  static serverSideInitial() {
+    const title = getHomeTitle();
+
+    return {
+      title,
+    };
+  }
+
+  render() {
+    const { initialSSRData: { title } } = this.props;
+
+    return (
+      <main className="container home">
+        <MetaTags title="divar-starter-kit 🚀" />
+        <img
+          className="home__logo"
+          src={logo}
+          alt="divar-stater-kit"
+        />
+        <p className="title">{title}</p>
+      </main>
+    );
+  }
 }
 
-export default Home;
+Home.propTypes = {
+  initialSSRData: PropTypes.shape({
+    title: PropTypes.string,
+  }).isRequired,
+};
+
+export default withSSRData(Home);
